78c260b48917b727e577d8fa0f78d92582982287
coopzsre <98057963+coopzsre@users.noreply.github.com>
2024-06-21 13:17:43 +0100

    update-locations

--- grafana-tf/main.tf
+++ grafana-tf/main.tf
@@ -1,30 +1,35 @@
+terraform {
+  required_providers {
+    grafana = {
+      source  = "grafana/grafana"
+      version = ">= 6.0.0"
+    }
+  }
+}
+
 provider "grafana" {
-  # Grafana provider configuration
-  api_url  = "http://<grafana-ip>:<grafana-port>"  # Replace with your Grafana IP and port
-  username = "admin"  # Replace with your Grafana admin username
-  password = "admin"  # Replace with your Grafana admin password
+  # Provider configuration (will be set dynamically in the GitHub Actions workflow)
 }
 
 resource "grafana_dashboard" "example_dashboard" {
   # Resource to define a Grafana dashboard
   provider = grafana
 
-  dashboard_json = <<EOF
-{
-  "editable": true,
-  "title": "Example Dashboard",
-  "panels": [
-    {
-      "type": "graph",
-      "title": "Example Graph Panel",
-      "targets": [
-        {
-          "refId": "A",
-          "expr": "example_metric{job=\"example\"}"
-        }
-      ]
-    }
-  ]
-}
-EOF
+  config_json = <<EOF
+  {
+    "editable": true,
+    "panels": [
+      {
+        "type": "graph",
+        "title": "Example Graph Panel",
+        "targets": [
+          {
+            "refId": "A",
+            "expr": "example_metric{job=\"example\"}"
+          }
+        ]
+      }
+    ]
+  }
+  EOF
 }
