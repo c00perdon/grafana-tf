41e614b29047807aa3f1349dba03f922f0ee176e
coopzsre <98057963+coopzsre@users.noreply.github.com>
2024-06-21 16:03:34 +0100

    grafana

---
+++ grafana-tf/terraform.tfvars
@@ -0,0 +1,4 @@
+grafana_url   = "http://127.0.0.1:60420"
+grafana_auth  = "admin:1qaz-2wsx"
+group_name    = "Example Group"
+group_email   = "example_group@example.com"
--- grafana-tf/variables.tf
+++ grafana-tf/variables.tf
@@ -9,3 +9,23 @@ variable "grafana_port" {
   description = "Port of Grafana instance"
   default = 3000  # Replace with your Grafana port
 }
+
+variable "grafana_url" {
+  description = "URL of the Grafana instance"
+  type        = string
+}
+
+variable "grafana_auth" {
+  description = "Authentication details for Grafana"
+  type        = string
+}
+
+variable "group_name" {
+  description = "Name of the Grafana user group"
+  type        = string
+}
+
+variable "group_email" {
+  description = "Email associated with the Grafana user group"
+  type        = string
+}
